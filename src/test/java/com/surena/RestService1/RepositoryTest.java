e25c36bb75acb29f4635caa68bf9491f4550f01b
FaramarzAf <faramarzafzali@gmail.com>
2021-06-23 13:22:23 +0430

    done

--- src/test/java/com/surena/RestService1/RepositoryTest.java
+++ src/test/java/com/surena/RestService1/RepositoryTest.java
@@ -3,52 +3,28 @@ package com.surena.RestService1;
 import com.surena.RestService1.model.User;
 import com.surena.RestService1.repository.UserRepository;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import javax.annotation.Resource;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
-/**
- * If you want to run the tests against the actually registered database,
- * you can annotate the test with @AutoConfigureTestDatabase(replace=Replace.NONE),
- * which will use the registered DataSource instead of an in-memory data source.
- */
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource({"classpath:application-test.properties"})
+@SpringBootTest
 public class RepositoryTest {
 
 
     @Autowired
     private UserRepository repository;
 
+    @After
+    public void clear() {
+        repository.deleteAll();
+    }
 
     @Test
     public void save_user() {
@@ -96,6 +72,7 @@ public class RepositoryTest {
     }
 
     @Test
+    @Transactional
     public void delete_by_id() {
         User user1 = new User(1L, "SamMJ", "0123", "Sam", "Johns");
         User user2 = repository.save(user1);
@@ -104,6 +81,7 @@ public class RepositoryTest {
     }
 
     @Test
+    @Transactional
     public void delete_by_username() {
         User user1 = new User(1L, "SamMJ", "0123", "Sam", "Johns");
         User user2 = repository.save(user1);
